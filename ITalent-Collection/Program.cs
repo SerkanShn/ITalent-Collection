03d429e59c80e50210687b5579cb003300ef9e3a
Serkan SAHIN <serkansahin114@gmail.com>
2022-08-14 19:36:32 +0300

    Liste tipinde CRUD işlemleri yapıldı

--- ITalent-Collection/Program.cs
+++ ITalent-Collection/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//---LIST---
+//String tip tutan bir liste örneği
+var stringList = new List<string>();
+
+//Listeye add fonksiyonu ile eleman ekleme
+stringList.Add("kalem");
+stringList.Add("defter");
+stringList.Add("kitap");
+
+//Listeden remove fonksiyonu ile eleman çıkarma
+stringList.Remove("defter");
+
+//Listeden belirli indeksdeki elemanı alma
+String temp = stringList[1];
+
+//Listede belirli indeksdeki elemanı güncelleme
+stringList[1] = "silgi";
+
+//Listedeki bütün elemanları yazdırma
+stringList.ForEach(x => Console.WriteLine(x));
+
+
+
+
+
+
+
+
